fd0c783fc1e9bfe3b129bb90d559b22ff08c5598
Santino Zahir Chamia <165103696+santinozc11@users.noreply.github.com>
2025-11-21 15:46:15 -0300

    Update header text from 'The Newcomers' to 'The Newcomersv1'

--- src/Views/Users/Listing.php
+++ src/Views/Users/Listing.php
@@ -21,7 +21,7 @@ class Listing
            -webkit-background-clip: text; 
            -webkit-text-fill-color: transparent;
            background-clip: text;">
-                        The Newcomers 
+                        The Newcomersv1 
                     </h1>
                     <?php
                     /** @var \Entity\User $user */
