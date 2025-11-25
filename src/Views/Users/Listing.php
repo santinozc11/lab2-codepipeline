00facb4843c9386bab05d035f976b102fb3dac18
JADev32 <113716455+JADev32@users.noreply.github.com>
2025-11-25 19:26:12 -0300

    Update header text from 'Newcomersv4' to 'Newcomersv5'

--- src/Views/Users/Listing.php
+++ src/Views/Users/Listing.php
@@ -21,7 +21,7 @@ class Listing
            -webkit-background-clip: text; 
            -webkit-text-fill-color: transparent;
            background-clip: text;">
-                        The Newcomersv4 
+                        The Newcomersv5 
                     </h1>
                     <?php
                     /** @var \Entity\User $user */
