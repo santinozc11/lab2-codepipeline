a3311db273954cc8a482e765f8d9f3fc0e1a8eb9
JADev32 <113716455+JADev32@users.noreply.github.com>
2025-11-27 10:44:08 -0300

    Update header text to 'The Newcomers - Test v2'

--- src/Views/Users/Listing.php
+++ src/Views/Users/Listing.php
@@ -21,7 +21,7 @@ class Listing
            -webkit-background-clip: text; 
            -webkit-text-fill-color: transparent;
            background-clip: text;">
-                        The Newcomers - Test
+                        The Newcomers - Test v2
                     </h1>
                     <?php
                     /** @var \Entity\User $user */
